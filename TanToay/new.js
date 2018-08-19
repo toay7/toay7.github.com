c864d72b94a47ec7f3c6e63b4d33b5589c96ca7a
toay7 <42510901+toay7@users.noreply.github.com>
2018-08-19 17:22:22 +0800

    Add files via upload

---
+++ TanToay/new.js
@@ -0,0 +1,17 @@
+alert("Hello world")
+
+alert(3+3)
+alert(3*3)
+alert(5%2)
+alert(4000%5000)
+alert(3+3)
+alert("3+3")
+alert("3"+"3")
+var firstname="Linus"
+var lastname="Cheng"
+alert(firstname+ " " +lastname)
+
+var first name="Linus"
+
+adding strings:
+"Hello" + "World" + "Age" + = "HelloWorldAge"
